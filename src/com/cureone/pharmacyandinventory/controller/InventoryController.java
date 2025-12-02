cbcd37425edbae2cc9dcb094caa91921a9fd65d6
AkifNaveed12 <akif27031@gmail.com>
2025-12-02 21:54:22 +0500

    Added Inventory class and completed testing of model package

--- src/com/cureone/pharmacyandinventory/controller/InventoryController.java
+++ src/com/cureone/pharmacyandinventory/controller/InventoryController.java
@@ -1,4 +1,19 @@
 package com.cureone.pharmacyandinventory.controller;
 
+import com.cureone.pharmacyandinventory.model.Category;
+import com.cureone.pharmacyandinventory.model.InventoryItem;
+import com.cureone.pharmacyandinventory.model.Medicine;
+
+import java.time.LocalDate;
+
 public class InventoryController {
+    public static void main(String[] args) {
+        Category category = new Category("Anti-Biotic", "It is an Anti-biotic medicine",1 );
+        Medicine medicine = new Medicine(112,"Hexaperamol", "Amc-Medical", "13/5/2025", 200.16, category, 30 );
+        InventoryItem inventoryItem = new InventoryItem(2, 20, 5, medicine, LocalDate.of(2025,5,13), "Shelf A6, Column 5", 200.16);
+
+        System.out.println(medicine.toString());
+        System.out.println(category.toString());
+        System.out.println(inventoryItem.toString());
+    }
 }
--- src/com/cureone/pharmacyandinventory/model/InventoryItem.java
+++ src/com/cureone/pharmacyandinventory/model/InventoryItem.java
@@ -1,4 +1,73 @@
 package com.cureone.pharmacyandinventory.model;
 
+import java.time.LocalDate;
+
 public class InventoryItem {
+
+    //this class holds the data memers of the inventory items their getter setters and the  constructors
+    private int itemId, quantity, minimumStockLimit;
+    private Medicine medicine;
+    private LocalDate expiryDate;
+    private String location;
+    private double price;
+
+    public InventoryItem(){}
+    public InventoryItem(int itemId, int quantity, int minimumStockLimit, Medicine medicine, LocalDate expiryDate, String location, double price) {
+        this.itemId = itemId;
+        this.quantity = quantity;
+        this.medicine = medicine;
+        this.expiryDate = expiryDate;
+        this.location = location;
+        this.price = price;
+        this.minimumStockLimit = minimumStockLimit;
+    }
+
+    public int getItemId(){
+        return this.itemId;
+    }
+    public void setItemId(int itemId){
+        this.itemId = itemId;
+    }
+    public int getQuantity(int quantity){
+        return this.quantity;
+    }
+    public void setQuantity(int quantity){
+        this.quantity = quantity;
+    }
+    public Medicine getMedicine(Medicine medicine){
+        return this.medicine;
+    }
+    public void setMedicine(Medicine medicine){
+        this.medicine = medicine;
+    }
+    public LocalDate getExpiryDate(LocalDate expiryDate){
+        return this.expiryDate;
+    }
+    public void setExpiryDate(LocalDate expiryDate){
+        this.expiryDate = expiryDate;
+    }
+    public String getLocation(){
+        return this.location;
+    }
+    public void setLocation(String location){
+        this.location = location;
+    }
+    public double getPrice(){
+        return this.price;
+    }
+    public void setPrice(double price){
+        this.price = price;
+    }
+    public int getMinimumStockLimit(){
+        return this.minimumStockLimit;
+    }
+    public void setMinimumStockLimit(int minimumStockLimit){
+        this.minimumStockLimit = minimumStockLimit;
+    }
+
+    public String toString(){
+        return "Inventory\n1.Name= " +medicine.getName() +"\n2.Quantity= " +quantity
+                +"\n3.MinimumStockLimit= " +minimumStockLimit +"\n4.ItemId= " +itemId +"\n5.Price= "+ price +
+                "\n6.Location= " +location+"\n7.ExpiryDate= " +expiryDate;
+    }
 }
